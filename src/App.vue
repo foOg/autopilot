b1c58bdc3fff190463f7a199d084e850c794360c
arnaud <arnaud@comet.co>
2021-07-07 10:35:09 +0200

    Update wording

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="app">
-    <h1>CTO Checklist</h1>
+    <h1>Autopilot</h1>
     <p>
       I there, I am <a href="https://www.linkedin.com/in/arnaud-aubry-74928730/" target="_blank">Arnaud Aubry</a>. 
       During my life as a CTO I found it difficult to be always on top of important topics.
-      This is why I built a CTO checklist. It is here to help CTO's handling the right thing at the right time. 
-      It is far from exhaustive as each company has its own struggles. If you want to contribute go and check <a href="https://github.com/foOg/autopilot">the github project</a>. 
-      The worldwide CTO community would thank you ;)
+      This is why I built a tech lead checklist. It is here to help tech team handling the right thing at the right time. 
+      It is far from exhaustive as each company has its own struggles. 
+      If you want to contribute go and check <a href="https://github.com/foOg/autopilot">the github project</a>. 
+      The worldwide tech community would thank you ;)
     </p>
     <div class="border"></div>
     <div class="filter">
@@ -32,7 +33,7 @@
     <h3>Want to contribute ?</h3>
     <p>
       Go and check <a href="https://github.com/foOg/autopilot">the github project</a> to add your items to the list. 
-    Don't hesitate to widely share this to your CTO friends. If it is not useful to you it might be for a rookie :)
+      Don't hesitate to widely share this to your CTO friends. If it is not useful to you it might be for a rookie :)
     </p>
   </div>
 </template>
